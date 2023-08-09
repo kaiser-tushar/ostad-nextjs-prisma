420f335943020dcde90243ea011dd47fd226cb6d
kaiser-tushar <kaiser.tushar@yahoo.com>
2023-08-09 18:15:49 +0600

    assignment-10 basic next.js app

---
+++ assignments/assignment-10/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/var/www/html/nextjs-prisma/assignments/assignment-10/src/app/layout\":[\"static/media/2aaf0723e720e8b9-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
